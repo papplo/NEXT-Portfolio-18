c16548fe896ae65e30cc8f2b633039cb3fed723c
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-08-13 12:47:31 +0200

    chore: added packages to support server side custom urls

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
+import fetch from 'isomorphic-unfetch'
+import sanity from '../lib/sanity'
+import sanityClient from '../lib/sanity'
+
+import Query from '../lib/query'
 
 const PostLink = (props) => (
   <li>
@@ -9,13 +14,31 @@ const PostLink = (props) => (
   </li>
 )
 
-export default () => (
+const Index = (props) => (
   <Layout>
     <h1>This is the title</h1>
     <ul>
-      <PostLink id="ux-ui-design" title="Ux/Ui-design"/>
-      <PostLink id="web-development" title="Web Development"/>
-      <PostLink id="other-projects" title="Other projects"/>
+      {props.posts.map(({post}) => (
+        <li key={post.id}>
+          <Link as={`/p/${post.id}`} href={`/post?id=${post.id}`}>
+            <a>{post.name} asdad </a>
+          </Link>
+        </li>
+      ))}
+      // <PostLink id="ux-ui-design" title="Ux/Ui-design"/>
+      // <PostLink id="web-development" title="Web Development"/>
+      // <PostLink id="other-projects" title="Other projects"/>
     </ul>
   </Layout>
 )
+
+Index.getInitialProps = async function() {
+  console.log(this.props);
+  // console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    posts: await sanity.fetch(Query)
+  }
+}
+
+export default Index
