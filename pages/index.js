27176b150bca72e7d3979ca9e6272d45582ed468
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-08-13 13:09:30 +0200

    feat: fetch with sanity-query

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import fetch from 'isomorphic-unfetch'
 import sanity from '../lib/sanity'
 import sanityClient from '../lib/sanity'
 
-import Query from '../lib/query'
-
 const PostLink = (props) => (
   <li>
     <Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
@@ -18,26 +16,26 @@ const Index = (props) => (
   <Layout>
     <h1>This is the title</h1>
     <ul>
-      {props.posts.map(({post}) => (
-        <li key={post.id}>
-          <Link as={`/p/${post.id}`} href={`/post?id=${post.id}`}>
-            <a>{post.name} asdad </a>
-          </Link>
-        </li>
-      ))}
-      // <PostLink id="ux-ui-design" title="Ux/Ui-design"/>
-      // <PostLink id="web-development" title="Web Development"/>
-      // <PostLink id="other-projects" title="Other projects"/>
+      {console.log(props.posts)}
+
+      <PostLink id="ux-ui-design" title="Ux/Ui-design"/>
+      <PostLink id="web-development" title="Web Development"/>
+      <PostLink id="other-projects" title="Other projects"/>
     </ul>
   </Layout>
 )
 
 Index.getInitialProps = async function() {
-  console.log(this.props);
-  // console.log(`Show data fetched. Count: ${data.length}`)
+  const query = '*[_type == "category"]'
 
   return {
-    posts: await sanity.fetch(Query)
+    posts: await sanityClient.fetch(query)
+      .then(category => {
+        console.log('---> fetched: ')
+        category.forEach(catName => {
+          console.log(`${catName.title}`);
+        })
+      })
   }
 }
 
