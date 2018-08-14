fd03b8bc0fadfb07667e32b730f2b0819b19bf49
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-08-14 05:27:21 +0200

    Feat: Repo no longer holds client (next)

--- schemas/schema.js
+++ schemas/schema.js
@@ -3,11 +3,12 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import blockContent from './blockContent'
 import category from './category'
+import links from './links'
 import post from './post'
 import works from './works'
 import author from './author'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([blockContent, post, author, category])
+  types: schemaTypes.concat([blockContent, post, author, works, links, category])
 })
